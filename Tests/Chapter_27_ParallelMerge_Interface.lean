77f83db59911c6e3e838fcd40faa7c86d2b98859
TankTechnology <2541826291@qq.com>
2026-08-05 05:14:42 +0800

    test(ch27): specify parallel merge algorithms

---
+++ Tests/Chapter_27_ParallelMerge_Interface.lean
@@ -0,0 +1,10 @@
+import CLRSLean.Chapter_27
+
+namespace CLRS.Chapter27
+
+#check binaryLowerBound
+#check binaryLowerBound_index_le_length
+#check binaryLowerBound_partition
+#check binaryLowerBound_work_le_log
+
+end CLRS.Chapter27
