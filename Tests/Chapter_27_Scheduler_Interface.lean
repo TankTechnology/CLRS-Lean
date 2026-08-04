52c7dbde321c9e4f78a5575c55dfd19c8ac2cb07
TankTechnology <2541826291@qq.com>
2026-08-05 00:59:42 +0800

    test(ch27): specify executable scheduler interface

---
+++ Tests/Chapter_27_Scheduler_Interface.lean
@@ -0,0 +1,15 @@
+import CLRSLean.Chapter_27
+
+namespace CLRS.Chapter27
+
+#check CompDAG.readyRun
+#check CompDAG.readyRun_subset_ready
+#check CompDAG.readyRun_card
+#check CompDAG.ready_nonempty_of_remainingWork_pos
+#check CompDAG.greedyStep
+#check CompDAG.greedyScheduleFrom
+#check CompDAG.greedySchedule
+#check CompDAG.greedySchedule_final_work_eq_zero
+#check CompDAG.greedySchedule_time_le_work_div_add_span
+
+end CLRS.Chapter27
