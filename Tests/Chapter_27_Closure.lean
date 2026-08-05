1c5c2e2da82b862fec0346dc0fe6f914de419231
TankTechnology <2541826291@qq.com>
2026-08-05 09:38:27 +0800

    test(ch27): seal main-text theorem boundary

---
+++ Tests/Chapter_27_Closure.lean
@@ -0,0 +1,45 @@
+import CLRSLean.Chapter_27
+
+namespace CLRS.Chapter27
+
+#check CompDAG.greedySchedule_final_work_eq_zero
+#check CompDAG.greedySchedule_time_le_work_div_add_span
+#check parallelLoop_span_le_log
+#check pAdd_correct
+#check pAddWork_allInput_bigTheta
+#check pAddSpan_allInput_bigTheta
+#check pMatMul_correct
+#check pMatMulExecWork_allInput_bigTheta
+#check pMatMulExecSpan_allInput_bigTheta
+#check pMerge_correct
+#check pMerge_childSize_le_threeQuarters
+#check pMerge_work_lower
+#check pMerge_work_upper
+#check pMerge_span_upper
+#check pMerge_interleaved_span_lower
+#check pMergeSort_correct
+#check pMergeSort_work_lower
+#check pMergeSort_work_upper
+#check pMergeSort_span_upper
+#check pMergeSort_worstFamily_span_lower
+#check strassenWork_allInput_bigTheta
+#check strassenSpan_allInput_bigTheta
+
+#print axioms CompDAG.greedySchedule_time_le_work_div_add_span
+#print axioms parallelLoop_span_le_log
+#print axioms pAdd_correct
+#print axioms pAddWork_allInput_bigTheta
+#print axioms pAddSpan_allInput_bigTheta
+#print axioms pMatMul_correct
+#print axioms pMatMulExecWork_allInput_bigTheta
+#print axioms pMatMulExecSpan_allInput_bigTheta
+#print axioms pMerge_correct
+#print axioms pMerge_childSize_le_threeQuarters
+#print axioms pMerge_work_upper
+#print axioms pMerge_interleaved_span_lower
+#print axioms pMergeSort_correct
+#print axioms pMergeSort_work_upper
+#print axioms pMergeSort_span_upper
+#print axioms pMergeSort_worstFamily_span_lower
+
+end CLRS.Chapter27
