639ccf0f905a288c1a4ee252c49509bd0dd92592
TankTechnology <2541826291@qq.com>
2026-08-20 11:45:08 +0800

    feat(ch34): close terminal statement status layouts

---
+++ Tests/Chapter_34_CookLevin_TransitionStmtTerminalStatus.lean
@@ -0,0 +1,9 @@
+import CLRSLean.Chapter_34.Section_34_4_NP_Completeness_Proofs.CookLevin.Circuitization.GeneratorTransitionInputCompiler
+
+open CLRS.Chapter34.Turing.CookLevin
+
+#check TransitionStmtTerminal.haltedWire
+#check TransitionStmtTerminal.labelWires
+#check transitionStmtOutputWires_terminal_status
+
+#print axioms transitionStmtOutputWires_terminal_status
