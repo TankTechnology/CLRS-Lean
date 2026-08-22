2fb7893972d2be95b992bf60c7ff7bfa900b876a
TankTechnology <2541826291@qq.com>
2026-08-22 10:55:46 +0800

    test(ch34): specify honest general CLIQUE interface

---
+++ Tests/Chapter_34_GeneralClique_Interface.lean
@@ -0,0 +1,30 @@
+import CLRSLean.Chapter_34
+
+namespace CLRS.Chapter34
+
+#check CliqueSym
+#check CliqueInstance
+#check CliqueInstance.WellFormed
+#check CliqueInstance.HasClique
+#check encodeCliqueInstance
+#check decodeCliqueInstance
+#check decode_encodeCliqueInstance
+#check encodeCliqueCertificate
+#check decodeCliqueCertificate
+#check decode_encodeCliqueCertificate
+#check GeneralCLIQUE
+#check mem_generalCLIQUE_iff
+#check cliqueVerifier
+#check cliqueVerifier_eq_true_iff
+#check mem_generalCLIQUE_iff_exists_certificate
+#check generalCLIQUE_polyTimeVerifiable
+#check generalCLIQUE_mem_ClassNP
+#check occurrenceCliqueInstance
+#check cnfSatisfiable_iff_occurrenceCliqueInstance
+#check threeCNFToGeneralCliqueMap
+#check threeCNFToGeneralCliqueMap_mem_iff
+#check Turing.TMClique.threeCNFSat_reducible_to_generalCLIQUE
+#check CLIQUE
+#check Turing.TMClique.threeCNFSat_reducible_to_CLIQUE
+
+end CLRS.Chapter34
