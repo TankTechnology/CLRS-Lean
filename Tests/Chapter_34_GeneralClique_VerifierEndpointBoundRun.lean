041a033a6e88da841c2a81cc9cf6a7552656b180
TankTechnology <2541826291@qq.com>
2026-08-24 00:26:36 +0800

    proof(ch34): connect CLIQUE endpoint checker

---
+++ Tests/Chapter_34_GeneralClique_VerifierEndpointBoundRun.lean
@@ -0,0 +1,9 @@
+import CLRSLean.Chapter_34.Section_34_4_NP_Completeness_Proofs.GeneralClique.VerifierMachine.EndpointBound.Run
+
+open CLRS Chapter34 StateTransition
+open CLRS.Chapter34.Turing.GeneralCliqueVerifier.EndpointBound
+
+#check targetFieldSteps
+#check vertexFieldSteps
+#check instanceSteps
+#check endpointBound_run
