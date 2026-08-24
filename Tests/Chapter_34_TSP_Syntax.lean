7cb145f6e60b42bfb31fa7664a65ba3f4591b69a
TankTechnology <2541826291@qq.com>
2026-08-25 06:01:33 +0800

    feat(ch34): validate TSP record syntax

---
+++ Tests/Chapter_34_TSP_Syntax.lean
@@ -0,0 +1,9 @@
+import CLRSLean.Chapter_34.Section_34_5_NP_Complete_Problems.TravelingSalesperson.VerifierMachine.Syntax
+
+#check CLRS.Chapter34.Turing.TSPVerifier.Syntax.instanceSyntax_encode
+#check CLRS.Chapter34.Turing.TSPVerifier.Syntax.certificateSyntax_encode
+#check CLRS.Chapter34.Turing.TSPVerifier.Syntax.instanceComputableInPolyTime
+#check CLRS.Chapter34.Turing.TSPVerifier.Syntax.certificateComputableInPolyTime
+
+#print axioms CLRS.Chapter34.Turing.TSPVerifier.Syntax.instanceComputableInPolyTime
+#print axioms CLRS.Chapter34.Turing.TSPVerifier.Syntax.certificateComputableInPolyTime
