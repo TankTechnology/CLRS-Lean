fc3a558e839d9d6530de0c31edb19aca4dac2a36
TankTechnology <2541826291@qq.com>
2026-08-24 15:50:02 +0800

    proof(ch34): assemble nonedge filter input

---
+++ Tests/Chapter_34_VertexCover_NonedgeInput.lean
@@ -0,0 +1,12 @@
+import CLRSLean.Chapter_34.Section_34_5_NP_Complete_Problems.VertexCover.ComplementMachine.NonedgeFilter
+
+open CLRS.Chapter34
+open CLRS.Chapter34.Turing
+open CLRS.Chapter34.Turing.VertexCover.ComplementMachine.NonedgeFilter
+
+#check inputStream
+#check inputStreamComputableInPolyTime
+#check complementEdges_eq_filter
+
+#print axioms inputStreamComputableInPolyTime
+#print axioms complementEdges_eq_filter
