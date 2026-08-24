6c77ddd919ae3016df565187abff799cba2c4d91
TankTechnology <2541826291@qq.com>
2026-08-25 05:36:48 +0800

    feat(ch34): prove decision-TSP NP-hard

---
+++ Tests/Chapter_34_TSP_Hardness.lean
@@ -0,0 +1,7 @@
+import CLRSLean.Chapter_34.Section_34_5_NP_Complete_Problems.TravelingSalesperson.Hardness
+
+#check CLRS.Chapter34.HAMCYCLE_reducible_to_TSP
+#check CLRS.Chapter34.TSP_npHard
+
+#print axioms CLRS.Chapter34.HAMCYCLE_reducible_to_TSP
+#print axioms CLRS.Chapter34.TSP_npHard
